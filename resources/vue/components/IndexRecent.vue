f3c53efb988dadeadd1031ec12d4235438b0c2bd
Nonuplet <130939038+nonuplet@users.noreply.github.com>
2023-10-20 18:49:33 +0900

    モバイルビューでのカルーセル内部実装（アニメーションは未完成）

--- resources/vue/components/IndexRecent.vue
+++ resources/vue/components/IndexRecent.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
 import { useBrowserStore } from '../../ts/stores/BrowserStore'
 import { useBlogStore } from '../../ts/stores/BlogStore'
+import { faCircleChevronLeft, faCircleChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { computed, ref } from 'vue'
+
+library.add(faCircleChevronLeft, faCircleChevronRight)
 
 const browser = useBrowserStore()
 const blog = useBlogStore()
+
+const current = ref(0)
+
+const prevActive = computed(() => {
+  return current.value > 0 ? 'active' : 'inactive'
+})
+
+const nextActive = computed(() => {
+  return current.value < blog.recent.length - 1 ? 'active' : 'inactive'
+})
+
+const onButtonClick = (i: number) => {
+  const next = current.value + i
+  if (next >= 0 && next < blog.recent.length) {
+    current.value = next
+  }
+}
 
 const getDateString = (date: Date): string => {
   return date.toLocaleDateString('ja-JP', {
@@ -25,7 +48,23 @@ const getDateString = (date: Date): string => {
         </h2>
       </div>
 
-      <div class="posts">
+      <div v-if="browser.isMobile" class="posts-mobile">
+        <div class="prev-button" :class="prevActive" @click="onButtonClick(-1)">
+          <font-awesome-icon :icon="['fas', 'circle-chevron-left']" size="2x" />
+        </div>
+        <div class="next-button" :class="nextActive" @click="onButtonClick(1)">
+          <font-awesome-icon :icon="['fas', 'circle-chevron-right']" size="2x" />
+        </div>
+        <div class="posts-mobile-container">
+          <a v-for="post of blog.recent" :key="post.url" :href="post.url" class="post">
+            <img class="post-image" :src="post.thumbnail" :alt="post.title" />
+            <p class="post-title">{{ post.title }}</p>
+            <p class="post-date">{{ getDateString(post.date) }}</p>
+          </a>
+        </div>
+      </div>
+
+      <div v-if="!browser.isMobile" class="posts-desktop">
         <a v-for="post of blog.recent" :key="post.url" :href="post.url" class="post">
           <img class="post-image" :src="post.thumbnail" :alt="post.title" />
           <p class="post-title">{{ post.title }}</p>
@@ -38,8 +77,9 @@ const getDateString = (date: Date): string => {
 
 <style scoped lang="sass">
 #recent
-  @apply w-full bg-white py-20
-  @apply px-3
+  @apply w-full bg-white
+  @apply py-10
+  @apply md:px-3 md:py-20
   @apply lg:px-10
 
   .recent-container
@@ -55,32 +95,58 @@ const getDateString = (date: Date): string => {
       .title
         @apply font-inter
         @apply text-4xl border-l-[0.7rem] pl-4 py-2
+        @apply max-md:ml-4
         @apply md:text-5xl md:border-l-[1rem] md:pl-5
 
-    .posts
+    .posts-mobile
+      @apply relative
+
+      .prev-button, .next-button
+        @apply absolute top-1/2 -translate-y-1/2
+
+        &.active
+          @apply text-primary
+
+        &.inactive
+          @apply text-gray
+
+      .prev-button
+        @apply left-4
+
+      .next-button
+        @apply right-4
+
+      .posts-mobile-container
+        @apply flex overflow-x-hidden py-10 gap-5
+
+        .post:first-of-type
+          @apply ml-[15vw]
+
+    .posts-desktop
       @apply px-2 w-full
       @apply grid grid-cols-3 gap-3
       @apply lg:gap-6 xl:gap-8
 
-      .post
-        @apply flex flex-col border border-gray shadow shadow-gray rounded-lg
+    .post
+      @apply flex flex-col border border-gray shadow shadow-gray rounded-lg
+      @apply max-md:max-w-[70vw] max-md:min-w-[70vw]
 
-        .post-image
-          @apply w-full rounded-t-lg
+      .post-image
+        @apply w-full rounded-t-lg
 
-        .post-texts
-          @apply grow px-1.5 py-1
+      .post-texts
+        @apply grow px-1.5 py-1
 
-        .post-title, .post-date
-          @apply px-1.5
+      .post-title, .post-date
+        @apply px-1.5
 
-        .post-title
-          @apply font-bold pt-1
-          @apply text-sm leading-tight
-          @apply lg:text-base lg:leading-tight
+      .post-title
+        @apply font-bold pt-1
+        @apply text-sm leading-tight
+        @apply lg:text-base lg:leading-tight
 
-        .post-date
-          @apply mt-auto text-gray text-right font-bold
-          @apply text-xs
-          @apply lg:text-sm
+      .post-date
+        @apply mt-auto text-gray text-right font-bold
+        @apply text-xs
+        @apply lg:text-sm
 </style>
